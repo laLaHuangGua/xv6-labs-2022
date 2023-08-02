ed90dcd8690cbc2e6aca931b8bfbc7570ac60b38
laLaHuangGua <1270562182@qq.com>
2023-08-02 19:16:11 +0800

    Util: finish xargs

---
+++ user/xargs.c
@@ -0,0 +1,36 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+#define MAX_CHAR_A_LINE_HOLD 255
+
+int
+main( int argc, char *argv[] )
+{
+  char buf[MAX_CHAR_A_LINE_HOLD];
+  char *p = buf;
+  char temp = 'a';
+
+  while ( read( 0, &temp, sizeof( char ) ) ) {
+    if ( (int)( temp ) != '\n' ) {
+      *p++ = temp;
+    } else {
+      *p = '\0';
+      if ( fork() == 0 ) {
+        char *args[argc + 1];
+        int i = 0;
+        for ( ; i < argc - 1; i++ ) {
+          args[i] = argv[i + 1];
+        }
+        args[i++] = buf;
+        args[i] = 0;
+        exec( argv[1], args );
+        exit( 0 );
+      } else {
+        wait( 0 );
+        p = buf;
+      }
+    }
+  }
+  exit( 0 );
+}
