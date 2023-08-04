4ffaa06ae3913208e9dd1f97353d6dbdf9a008e2
laLaHuangGua <1270562182@qq.com>
2023-08-04 16:06:39 +0800

    Syscall: finish trace

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -91,3 +91,15 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 
+sys_trace(void)
+{
+  int mask;
+  
+  argint(0, &mask);
+  if(mask < 2)
+    return -1;
+  myproc()->mask = mask;
+  return 0;
+}
