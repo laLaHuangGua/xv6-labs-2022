b5cadd7fee7a4ffded4bc1aabd8d3455e1d19b89
laLaHuangGua <1270562182@qq.com>
2023-08-12 16:22:08 +0800

    Traps: finish backtrace

--- kernel/printf.c
+++ kernel/printf.c
@@ -123,6 +123,7 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  backtrace();
   for(;;)
     ;
 }
@@ -132,4 +133,17 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace(void)
+{
+  uint64 fp = r_fp();
+  uint64 bound = PGROUNDDOWN(fp) + PGSIZE;
+
+  printf("backtrace:\n");
+  while (fp < bound) {
+    printf("%p\n", *((uint64*)(fp - 8)));
+    fp = *((uint64*)(fp - 16));
+  }
 }
