98d5388bb0e27878251e4a9c4d8dddf6d38de84d
laLaHuangGua <1270562182@qq.com>
2024-02-29 09:02:38 +0800

    Util: rewrite pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,29 +3,27 @@
 #include "user/user.h"
 
 int
-main( int argc, char *argv[] )
+main(int argc, char *argv[])
 {
   char buf[1];
 
   int p[2];
-  pipe( p );
+  pipe(p);
 
-  if ( fork() == 0 ) {
-    if ( read( p[0], buf, 1 ) ) {
-      printf( "%d: received ping\n", getpid() );
-      write( p[1], "b", 1 );
-    }
-    close( p[0] );
-    close( p[1] );
-    exit( 0 );
+  if (fork() == 0) {
+    if (read(p[0], buf, 1))
+      printf("%d: received ping\n", getpid());
+    write(p[1], "b", 1);
+    close(p[0]);
+    close(p[1]);
+    exit(0);
   } else {
-    write( p[1], "a", 1 );
-    wait( 0 );
-    if ( read( p[0], buf, 1 ) ) {
-      printf( "%d: received pong\n", getpid() );
-    }
-    close( p[0] );
-    close( p[1] );
+    write(p[1], "a", 1);
+    wait(0);
+    if (read(p[0], buf, 1))
+      printf("%d: received pong\n", getpid());
+    close(p[0]);
+    close(p[1]);
   }
-  exit( 0 );
+  exit(0);
 }
