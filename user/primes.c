a3b2475a23812df84014a9c4613c0db5b782b8a0
laLaHuangGua <1270562182@qq.com>
2024-02-29 10:32:11 +0800

    Util: rewrite prime

--- user/primes.c
+++ user/primes.c
@@ -5,42 +5,35 @@
 #define UPPER_BOUND 36
 
 int
-main( int argc, char *argv[] )
+main(int argc, char *argv[])
 {
-  int p[2];
-  pipe( p );
-
-  for ( int i = 3; i < UPPER_BOUND; i++ ) {
-    write( p[1], &i, 1 );
-  }
+  int prime;
   int bound = UPPER_BOUND;
-  write( p[1], &bound, 1 );
 
-  int prime = 2;
-  printf( "prime %d\n", prime );
+  int p[2];
+  pipe(p);
 
-  while ( 1 ) {
-    if ( fork() == 0 ) {
+  for (int i = 2; i <= UPPER_BOUND; i++)
+    write(p[1], &i, 1);
+
+  while (1) {
+    if (!(read(p[0], &prime, 1) && prime < UPPER_BOUND))
+      break;
+    printf("prime %d\n", prime);
+    if (fork() == 0) {
       int num = 0;
-      while ( read( p[0], &num, 1 ) && num < UPPER_BOUND ) {
-        if ( num % prime != 0 ) {
-          write( p[1], &num, 1 );
-        }
-      }
-      write( p[1], &bound, 1 );
-      close( p[0] );
-      close( p[1] );
-      exit( 0 );
+      while (read(p[0], &num, 1) && num < UPPER_BOUND)
+        if (num % prime != 0) 
+          write(p[1], &num, 1);
+      write(p[1], &bound, 1);
+      close(p[0]);
+      close(p[1]);
+      exit(0);
     } else {
-      wait( 0 );
-      if ( read( p[0], &prime, 1 ) && prime < UPPER_BOUND ) {
-        printf( "prime %d\n", prime );
-      } else {
-        break;
-      }
+      wait(0);
     }
   }
-  close( p[0] );
-  close( p[1] );
-  exit( 0 );
+  close(p[0]);
+  close(p[1]);
+  exit(0);
 }
