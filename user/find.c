93507467534381799c4c746409125343aa88933c
laLaHuangGua <1270562182@qq.com>
2024-02-29 19:29:24 +0800

    Util: reformat find

--- user/find.c
+++ user/find.c
@@ -4,52 +4,53 @@
 #include "kernel/fs.h"
 
 void
-find( char *path, const char *name )
+find(char *path, const char *name)
 {
   char buf[512], *p;
   int fd;
   struct dirent de;
   struct stat st;
 
-  if ( ( fd = open( path, 0 ) ) < 0 ) {
-    fprintf( 2, "find: cannot open %s\n", path );
+  if ((fd = open(path, 0)) < 0) {
+    fprintf(2, "find: cannot open %s\n", path);
     return;
   }
 
-  if ( fstat( fd, &st ) < 0 ) {
-    fprintf( 2, "find: cannot stat %s\n", path );
-    close( fd );
+  if (fstat(fd, &st) < 0) {
+    fprintf(2, "find: cannot stat %s\n", path);
+    close(fd);
     return;
   }
 
-  if ( strlen( path ) + 1 + DIRSIZ + 1 > sizeof buf ) {
-    printf( "find: path too long\n" );
-  }
+  if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf)
+    printf("find: path too long\n");
 
-  strcpy( buf, path );
-  p = buf + strlen( buf );
+  strcpy(buf, path);
+  p = buf + strlen(buf);
   *p++ = '/';
-  while ( read( fd, &de, sizeof( de ) ) == sizeof( de ) ) {
-    if ( de.inum == 0 )
+  while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+    if (de.inum == 0)
       continue;
-    memmove( p, de.name, DIRSIZ );
+    memmove(p, de.name, DIRSIZ);
     p[DIRSIZ] = 0;
-    if ( stat( buf, &st ) < 0 ) {
-      printf( "find: cannot stat %s\n", buf );
+    if (stat(buf, &st) < 0) {
+      printf("find: cannot stat %s\n", buf);
       continue;
     }
-    if ( st.type == T_FILE && !strcmp( de.name, name ) ) {
-      printf( "%s\n", buf );
-    } else if ( st.type == T_DIR && strcmp( de.name, "." ) && strcmp( de.name, ".." ) ) {
-      find( buf, name );
+    if (st.type == T_FILE && strcmp(de.name, name) == 0) {
+      printf("%s\n", buf);
+    } else if (st.type == T_DIR 
+                && strcmp(de.name, ".") != 0 
+                && strcmp(de.name, "..") != 0) {
+      find(buf, name);
     }
   }
-  close( fd );
+  close(fd);
 }
 
 int
-main( int argc, char *argv[] )
+main(int argc, char *argv[])
 {
-  find( argv[1], argv[2] );
-  exit( 0 );
+  find(argv[1], argv[2]);
+  exit(0);
 }
